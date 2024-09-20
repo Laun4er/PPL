ac50e02a5700554200ac71321a07e6772779066e
Laun4er <homecdenis5@icloud.com>
2024-09-20 17:23:43 +0500

    Добавлен аргумент цели объекта для работы NativeUpdater

--- PPL/MainWindow.xaml.cs
+++ PPL/MainWindow.xaml.cs
@@ -1,15 +1,42 @@
-﻿using System.Windows;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Windows;
 
 namespace PPL
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            Environment.GetCommandLineArgs().ToList().ForEach(x =>
+            {
+                if (x.EndsWith("/Upload"))
+                {
+                    try
+                    {
+                        JObject jsonObject = new JObject();
+
+                        foreach (SettingsProperty prop in Properties.Settings.Default.Properties)
+                        {
+                            jsonObject[prop.Name] = Properties.Settings.Default[prop.Name]?.ToString();
+                        }
+
+                        string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "settings.json");
+
+                        File.WriteAllText(filePath, jsonObject.ToString());
+
+                        this.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"{ex.Message} \n \n {ex.InnerException}");
+                    }
+                }
+            });
         }
     }
 }
